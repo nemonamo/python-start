24f36d9ed0002470235dd2c29362f29dcd3fba31
nemonamo <namho0941@daum.net>
2022-07-18 18:06:25 +0900

    string

---
+++ stringrule.py
@@ -0,0 +1,25 @@
+print('>>> multiline = "Life is too short\\nYou need python" ')
+print(">>> multiline='''\n... Life is too short\n... You need python\n... '''")
+
+print(">>> head = \"Python\" ")
+print(">>> head = \" is fun!\" ")
+print(">>> head + tail")
+b = "'"
+
+head = "Python"
+tail = " is fun!"
+print(b+head + tail+b)
+
+print(">>> a = \"Python\" ")
+a = "Python"
+print(">>> a * 2")
+print(b+a*2+b)
+
+print('>>> a = "Life is too short, You Python"')
+a = "Life is too short, You Python"
+print(">>> a[0]")
+print(b+a[0 ]+b)
+print(">>> a[12]")
+print(b+a[12]+b)
+print(">>> a[-1]")
+print(b+a[-1]+b)
